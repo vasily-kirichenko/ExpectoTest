5bad0596c55abc6e0688fd17ba456451cf9ed4cb
Vasily Kirichenko <vaskir@gmail.com>
2017-04-04 20:46:11 +0300

    Add project files.

---
+++ ExpectoTest/Program.fs
@@ -0,0 +1,17 @@
+﻿open Expecto
+open System
+
+let tests =
+    testList "Simple tests" [
+        yield test "Simple" { Expect.containsAll [0; 1; 3; 2] [1; 2] "foo" }
+        yield!
+            testParam (2, 4) [
+                "One", fun (n, dn) () -> Expect.equal (n + n) dn (sprintf "%d + %d = %d" n n dn)
+            ]
+    ]
+
+[<EntryPoint>]
+let main argv = 
+    runTestsWithArgs defaultConfig argv tests |> ignore
+    Console.ReadKey() |> ignore
+    0
